03deac880a81a3a9719d2e0dd50a208a7a4e228f
codelockinfo <codelockinfo@gmail.com>
2024-01-30 15:32:24 +0530

    Change file

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE"); // Adjust the allowed methods
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Add the necessary headers
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
