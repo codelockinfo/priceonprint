1f39fcfdc484a3d7a4cb379cdf9697f1a56a989e
codelockinfo <codelockinfo@gmail.com>
2024-01-30 15:56:22 +0530

    Change logs

--- connection.php
+++ connection.php
@@ -10,8 +10,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connection Success";
 }
-
 // Perform database operations here...
 
 // Close the connection
--- index.php
+++ index.php
@@ -9,7 +9,6 @@ include 'connection.php';
 
 
 echo "<pre>";
-print_r($_POST);
 print_r($_FILES);
 echo "</pre>";
 
