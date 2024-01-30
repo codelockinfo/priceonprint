4df70cdd5a5320f1c49fe8b6ba98b9b5f4650b31
codelockinfo <codelockinfo@gmail.com>
2024-01-30 15:53:23 +0530

    database connection

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "u402017191_printPrice";
+$password = "Codelock@99";
+$dbname = "u402017191_printonprice";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Perform database operations here...
+
+// Close the connection
+$conn->close();
+?>
--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE"); // Adjust the allowed methods
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Add the necessary headers
+
+
+include 'connection.php';
+
+
 echo "<pre>";
 print_r($_POST);
 print_r($_FILES);
